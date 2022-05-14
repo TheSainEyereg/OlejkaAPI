27e01985bfca56766c0e0af741ea3d8fccdd6512
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2022-05-14 19:45:15 +0300

    Added figlet (aka ASCII API)

--- index.js
+++ index.js
@@ -265,4 +265,34 @@ app.get("/discord/embed/oembed_gen/", async(req,res) => {
 	 });
 })
 
+/*   ~~~   FIGLET   ~~~   */
+const figlet = require("figlet");
+
+app.get("/figlet/", async(req,res) => {
+	const text = req.query.text
+	const font = req.query.font?.replace(/.?.(\/|\\)/gi, "") || "big";
+	
+	if (!text) return res.status(400).json({error: "No text specified"});
+
+	figlet.text(text, {font}, (err, data) => {
+		if (err) {
+			if (err.message.includes("ENOENT: no such file or directory") && err.message.includes(`${font}.flf`)) return res.status(404).json({
+				error: `Font ${font} was not found!`,
+				errorDetails: {
+					fonts: figlet.fontsSync()
+				}
+			});
+			return res.status(500).json({
+				error: "Unknown error!",
+				errorDetails: err.message
+			});
+		}
+
+		res.json({
+			text: data,
+			font: font
+		});
+	})
+})
+
 /*   ~~~   ~~~   ~~~   */
