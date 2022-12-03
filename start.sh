0e533108a5e98199f65d4d884adfb364241f3170
Olejka <oleglogwinow@gmail.com>
2022-12-03 03:27:03 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 echo "Starting API.  To view window type screen -r APIv2."
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
 cd /home/ubuntu/olejka.ru_api/v2/
-/bin/screen -dmS APIv2 /home/ubuntu/.nvm/versions/node/v17.0.1/bin/node index.js
+/bin/screen -dmS APIv2 /home/ubuntu/.nvm/versions/node/v17.0.1/bin/node src/app.js
